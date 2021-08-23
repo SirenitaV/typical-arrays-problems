6c84b4289c9ca27ea7d4cc8db515e954955ab86b
SirenitaV <sirenita.violetta@gmail.com>
2021-08-23 21:31:36 +0500

    feat: solve arrays task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+	
+	if (array === undefined || array.length === 0) { 
+	         return 0
+	           }
+			else
+			{
+            return Math.min.apply(null, array);
+			}
+   
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array === undefined || array.length === 0) { 
+	         return 0
+	           }
+			   else {
+	return Math.max.apply(null, array);
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+    }
+
+else {
+let total = 0;
+let sum = 0;
+
+array.forEach(function(item, index) {
+ total += item;
+ sum++;
+}); 
+
+return total/sum;
 }
+}
+
